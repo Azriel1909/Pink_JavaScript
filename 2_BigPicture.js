9ddc3ae88c8ed800f515fffd38d18fbbf7c7753f
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-10-31 21:27:23 -0600

    Big Picture

---
+++ 2_BigPicture.js
@@ -0,0 +1,12 @@
+# Node.js
+1. We can write server-side software in JavaScript
+2. We can write software for the command line
+3. Node's software registry, npm, has become the dominant way of installing tools and libraries.
+
+### Structure of browsers and Node.js
+|JS Standard|Platform API|
+|----|---|
+|JavaScript Engine|Platform Core|
+
+The foundational layer consists of the *JavaScript Engine* and *Platform-Specific Core *functionality*.
+Two APIs are hosted on top of this foundation. 
