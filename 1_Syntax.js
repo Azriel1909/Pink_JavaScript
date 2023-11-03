a530172eb1cf100e9a394841cea446b86c4c1500
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-02 21:58:26 -0600

    Js Syntax: Assertions

--- 1_Syntax.js
+++ 1_Syntax.js
@@ -29,3 +29,15 @@ false
 
 'abc'
 'Ximena Toledo'
+
+// > Assertions: Describes what the result of a computation is expected to look like and throws an exception if those exceptions are not correct.
+
+// assert.equal(7+1,8)
+
+// * Login to the console
+
+// - Printing a value to standard out
+console.log('Hi, I am Ximena Toledo')
+
+// - Printing erro information to standard error
+console.error('Oh no, something went wrong!')
