5934d5abe5a2ab99badfe9d41a2ebe787d207916
Ximena Toledo <xtoledor1900@alumno.ipn.mx>
2023-11-02 21:54:39 -0600

    Js Syntax: Atomic Values

---
+++ 1_Syntax.js
@@ -0,0 +1,31 @@
+// > Basic Constructs
+
+// * Comments
+
+// Single-line comment
+
+/**
+ * Comment with multiple lines
+*/
+
+// > Primitive Atomic Values
+
+// * Booleans
+
+true
+false
+
+// * Numbers
+
+1.141
+-123
+
+// * BigInts
+
+17n
+-49n
+
+// * Strings
+
+'abc'
+'Ximena Toledo'
